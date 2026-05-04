4e33fb57a8f940aec34be1d2af29242b12fb6032
lichnguyen0 <fakesangmy@gmail.com>
2026-05-04 21:28:01 +0700

    create project

---
+++ src/main/java/com/notemanagementsystem/NoteManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.notemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/notemanagementsystem/NoteManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
